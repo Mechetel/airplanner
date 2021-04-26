4656bb4594eeb5d1b726c67e931bbbfebc369bc6
mechetel <dima.homa5@gmail.com>
2021-04-26 21:22:52 +0300

    update webpacker

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -20,7 +20,7 @@ Hello.propTypes = {
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="Dima" />,
+    <Hello name="React" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
