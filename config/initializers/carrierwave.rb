291864b2e4dce551106beb9803a89d6fd0d2464a
mechetel <dima.homa5@gmail.com>
2021-06-06 19:56:13 +0300

    feat: update carrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,24 +1,16 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    # Configuration for Amazon S3
-    :provider              => 'AWS',
-    :aws_access_key_id     => ENV['AWS_ACCESS_KEY_ID'],
-    :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-    :region                => ENV['S3_REGION']
-  }
-
-  # For testing, upload files to local `tmp` folder.
+ CarrierWave.configure do |config|
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
-    config.enable_processing = false
-    config.root = "#{Rails.root}/tmp"
   else
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
     config.storage = :fog
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => ENV['S3_REGION'],
+    }
+    config.fog_directory = ENV['S3_BUCKET_NAME']
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
   end
-
-  config.cache_dir = Rails.root.join('/tmp/uploads')
-
-  config.fog_directory    = ENV['S3_BUCKET_NAME']
-  config.s3_access_policy = :public_read
-  config.fog_host         = "#{ENV['S3_HOST_NAME']}/#{ENV['S3_BUCKET_NAME']}"
 end
